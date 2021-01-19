8332ce94d057fe680e2cdb0e724ab8634fd4dfd8
Wendi Magee <wendimagee@gmail.com>
2021-01-19 15:07:26 -0500

    I've got steps 1 and 2 done from the assignment. User input and calculations are done.

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -7,7 +7,20 @@ namespace Lab2
         static void Main(string[] args)
         {
             Console.Write("Enter Length: ");
-            string length = Console.ReadLine();
+            string string1 = Console.ReadLine();
+            float length = float.Parse(string1);
+
+            Console.Write("Enter Width: ");
+            string string2 = Console.ReadLine();
+            float width = float.Parse(string2);
+
+            Console.Write("Enter Height: ");
+            string string3 = Console.ReadLine();
+            float height = float.Parse(string3);
+
+            float area = length * width;
+            Console.WriteLine("The area is " + area);
+
 
         }
     }
