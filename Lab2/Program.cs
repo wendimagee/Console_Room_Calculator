3c56b6651667ed6d2149bd641953cc84f3fd37a7
Wendi Magee <wendimagee@gmail.com>
2021-01-19 14:53:50 -0500

    initial commit

---
+++ Lab2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Length: ");
+            string length = Console.ReadLine();
+
+        }
+    }
+}
